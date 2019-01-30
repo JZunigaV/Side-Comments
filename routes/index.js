44d36f650ce6a29977b2b1c2937a1144fbc7d952
JzunigaV <j_zuniga4@hotmail.com>
2019-01-30 13:00:02 -0600

    added new logic to check existing comments

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,17 @@ const Section = require("../models/section");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+
   var commentas = [];
 
-  Section.findOne({
-      sectionId: 2
-    })
-    .then(comment => {
-      console.log(comment);
-      commentas = comment;
+  Section.find()
+    .then(comments => {
+
+      commentas = comments;
     })
     .catch(err => {
       console.log(err);
-    });
+    })
 
   User.findOne({
       id: 1
@@ -26,7 +25,8 @@ router.get("/", (req, res, next) => {
         userr: user,
         coment: commentas
       };
-      console.log(data.coment);
+
+      console.log("YThe data is:", data);
       res.render("index", data);
     })
     .catch(err => {
@@ -55,7 +55,6 @@ router.post("/comments", (req, res, next) => {
     sectionId
   } = req.body;
 
-
   //Assign he body to a variable
   const data = {
     authorAvatarUrl,
@@ -77,99 +76,34 @@ router.post("/comments", (req, res, next) => {
     replies: []
   }
 
-  //Search the requested section on the database
-  Section.findOne({
+
+  //Query 
+  const query = {
       sectionId: sectionId
-    })
-    .then(sectionFound => {
+    },
+    update = {
+      $push: {
+        comments: newComment
+      }
+    },
+    options = {
+      upsert: true,
+      new: true,
+      setDefaultsOnInsert: true
+    };
 
-      sectionFound.comments.push(newComment);
-
-      Section.update({
-          _id: sectionFound._id
-        }, {
-          $set: {
-            sectionId: sectionId,
-            comments: sectionFound.comments
-          }
-        })
-        .then((book) => {
-          console.log(book)
-        })
-        .catch((error) => {
-          console.log(error);
-        })
+  //if the document is not found, then create a new one, else update comments and push the newCommen
+  Section.findOneAndUpdate(query, update, options)
+    .then(comment => {
+      res.redirect("/");
     })
     .catch(err => {
       console.log(err);
-    });
+    })
 });
 
 
-// //if this sectionId already exists on the database, we need to push the comment to the array 
-// Section.findOne({
-//     sectionId: sectionId
-//   })
-//   .then(section => {
-
-//     //the section Id was found on the database, push the new comment inside it's array
-//     section.comments.push(data);
-//     var newComment = section.comments;
-
-//     //then update the schema on the database
-//     Section.update({
-//         sectionId: sectionId
-//       }, {
-//         $set: {
-//           sectionId: "que tranza",
-
-//         }
-//       })
-//       .then((book) => {
-//         console.log(book)
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       })
 
 
-
-//   })
-//   .catch(err => console.log(err));
-
-// const {
-//   sectionId,
-//   comment,
-//   authorAvatarUrl,
-//   authorName,
-//   authorId,
-//   authorUrl,
-//   id
-// } = req.body;
-
-// const newComment = {
-//   id: id,
-//   authorAvatarUrl: authorAvatarUrl,
-//   authorName: authorName,
-//   authorId: authorId,
-//   authorUrl: authorUrl,
-//   comment: comment
-// };
-// const newSection = new Section({
-//   sectionId: req.body.sectionId
-// });
-
-// newSection.comments.push(newComment);
-
-// console.log(newSection);
-
-// newSection
-//   .save()
-//   .then(comment => {
-//     res.redirect("/test");
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
 
 module.exports = router;
