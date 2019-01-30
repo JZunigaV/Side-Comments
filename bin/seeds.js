e6e57bd4e4f4cc473411abf0d5ea200a9ace0e66
JzunigaV <j_zuniga4@hotmail.com>
2019-01-30 11:35:18 -0600

    Se agrega el psuh a array de comments

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 
-const dbName = 'awesome-sidecomments';
+const dbName = 'sidecomments';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
-const users = [
-  {
-    id: 1,
-    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDW3D0Emu0_gpP-tAEGPjW88zSabvpdICv6BaoNLArqY3xB4NA",
-    name: "Jesus Zuñiga Vega"
-  },
-]
+const users = [{
+  id: 1,
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDW3D0Emu0_gpP-tAEGPjW88zSabvpdICv6BaoNLArqY3xB4NA",
+  name: "Jesus Zuñiga Vega"
+}, ]
 
 User.create(users, (err) => {
-  if (err) { throw(err) }
+  if (err) {
+    throw (err)
+  }
   console.log(`Created ${users.length} users`)
   mongoose.connection.close()
 });
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
-const Comment = require("../models/comment");
+const Section = require("../models/section");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   var commentas = [];
 
-  Comment.findOne({ authorName: "Whiteskullka" })
+  Section.findOne({
+      sectionId: 2
+    })
     .then(comment => {
+      console.log(comment);
       commentas = comment;
     })
     .catch(err => {
       console.log(err);
     });
 
-  User.findOne({ id: 4 })
+  User.findOne({
+      id: 1
+    })
     .then(user => {
       const data = {
         userr: user,
@@ -29,7 +34,6 @@ router.get("/", (req, res, next) => {
     });
 });
 
-//Test route
 router.get("/test", (req, res, next) => {
   const data = {
     name: "jesus"
@@ -39,17 +43,133 @@ router.get("/test", (req, res, next) => {
 
 //Comment posted
 router.post("/comments", (req, res, next) => {
-  //Save the comment on database
-  const test = req.body;
-  res.send({ msg: test });
 
-  // newComment
-  //   .save()
-  //   .then(book => {
-  //     res.redirect("/index");
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
+  //Get the request body
+  const {
+    authorAvatarUrl,
+    authorId,
+    authorName,
+    authorUrl,
+    comment,
+    id,
+    sectionId
+  } = req.body;
+
+
+  //Assign he body to a variable
+  const data = {
+    authorAvatarUrl,
+    authorId,
+    authorName,
+    authorUrl,
+    comment,
+    sectionId,
+    id
+  }
+
+  //Comment object
+  const newComment = {
+    authorAvatarUrl,
+    authorName,
+    authorId,
+    authorUrl,
+    comment,
+    replies: []
+  }
+
+  //Search the requested section on the database
+  Section.findOne({
+      sectionId: sectionId
+    })
+    .then(sectionFound => {
+
+      sectionFound.comments.push(newComment);
+
+      Section.update({
+          _id: sectionFound._id
+        }, {
+          $set: {
+            sectionId: sectionId,
+            comments: sectionFound.comments
+          }
+        })
+        .then((book) => {
+          console.log(book)
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+
+
+// //if this sectionId already exists on the database, we need to push the comment to the array 
+// Section.findOne({
+//     sectionId: sectionId
+//   })
+//   .then(section => {
+
+//     //the section Id was found on the database, push the new comment inside it's array
+//     section.comments.push(data);
+//     var newComment = section.comments;
+
+//     //then update the schema on the database
+//     Section.update({
+//         sectionId: sectionId
+//       }, {
+//         $set: {
+//           sectionId: "que tranza",
+
+//         }
+//       })
+//       .then((book) => {
+//         console.log(book)
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+
+
+
+//   })
+//   .catch(err => console.log(err));
+
+// const {
+//   sectionId,
+//   comment,
+//   authorAvatarUrl,
+//   authorName,
+//   authorId,
+//   authorUrl,
+//   id
+// } = req.body;
+
+// const newComment = {
+//   id: id,
+//   authorAvatarUrl: authorAvatarUrl,
+//   authorName: authorName,
+//   authorId: authorId,
+//   authorUrl: authorUrl,
+//   comment: comment
+// };
+// const newSection = new Section({
+//   sectionId: req.body.sectionId
+// });
+
+// newSection.comments.push(newComment);
+
+// console.log(newSection);
+
+// newSection
+//   .save()
+//   .then(comment => {
+//     res.redirect("/test");
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
 module.exports = router;
